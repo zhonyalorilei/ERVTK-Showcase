06ef3d30f660cbe9f98780a30cd1a1f559afffc9
IamRodeeel <rodel.alipuso01@gmail.com>
2024-05-16 14:42:38 +0800

    <<<< Navigation responsive

--- scripts.js
+++ scripts.js
@@ -16,3 +16,17 @@ function reveal () {
 
     }
 }
+/*-- Responsive Navigation---*/
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
